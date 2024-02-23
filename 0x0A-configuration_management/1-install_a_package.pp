0ef9df67c4e67d68f15f51e0d938bb8ab145ac73
Tau <taumasaire@gmail.com>
2024-02-23 13:17:51 +0200

    install a package manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # installs flask from pip3
 
+package { 'python3-pip':
+    ensure => installed,
+}
+
 package { 'flask':
-    ensure   => '2.1.0'
-    provider => 'pip3',
+    ensure   => '2.1.0',
+    provider => pip3,
+    require  => Package['python3-pip'],
 }
