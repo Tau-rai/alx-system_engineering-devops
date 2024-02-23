53aeca470a033eedf5eba4951a5e18384b19c59e
Tau <taumasaire@gmail.com>
2024-02-23 14:21:33 +0200

    updating manifest package resource

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,8 @@ package { 'python3-pip':
     ensure => installed,
 }
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => pip3,
-    require  => Package['python3-pip'],
+exec { 'flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+    require => Package['python3-pip'],
 }
