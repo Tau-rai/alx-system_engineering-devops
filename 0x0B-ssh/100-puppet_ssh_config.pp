dd1f022214d94fe47febc2b14d462a075014a2ce
Tau <taumasaire@gmail.com>
2024-02-24 21:12:54 +0200

    puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# makes changes to the client configuration file
+
+file { '/home/tau_rai/.ssh/config':
+    ensure  => file,
+    owner   => 'tau_rai',
+    group   => 'tau_rai',
+    mode    => '0600',
+    content => "
+     Host *
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+    ",
+}
