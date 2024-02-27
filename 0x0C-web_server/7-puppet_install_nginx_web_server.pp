3c8f32138b8a14c157fec842ef104d4e440d6196
Tau <taumasaire@gmail.com>
2024-02-27 13:54:00 +0200

    install nginx with puppet manifest

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,46 @@
+# installs and configures a Nginx web server with puppet
+
+unless defined(Class['nginx']) {
+    # install nginx module if not already installed
+    package { 'nginx_module':
+        ensure   => 'installed',
+        provider => 'puppet_gem',
+    }
+}
+
+include nginx
+
+
+class { 'nginx': }
+
+nginx::resource::server { 'default':
+  listen_port          => 80,
+  www_root             => '/var/www/html',
+  index_files          => ['index.html'],
+  use_default_location => false,
+  location_cfg_append  => {
+    'return' => '301 /redirect_me',
+  },
+  locations            => {
+    '/'            => {
+      location            => '/',
+      www_root            => '/var/www/html',
+      index_files         => ['index.html'],
+      location_cfg_append => {
+        'return' => '200 "Hello World!\n"',
+      },
+    },
+    '/redirect_me' => {
+      location            => '/redirect_me',
+      location_cfg_append => {
+        'return' => '301 /',
+      },
+    },
+  },
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Class['nginx'],
+}
